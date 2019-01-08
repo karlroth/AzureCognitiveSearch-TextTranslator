0df464508c63c37dba4b6538e687f7ef59ce2290
Karl Roth <karoth4@illinois.edu>
2019-01-08 09:48:06 -0600

    First commit. Translates documents below character limit

---
+++ TranslateFunction.cs
@@ -0,0 +1,146 @@
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace TranslateFunction
+{
+    // This function will simply translate messages sent to it.
+    public static class Function1
+    {
+        #region classes used to serialize the response
+        private class WebApiResponseError
+        {
+            public string message { get; set; }
+        }
+
+        private class WebApiResponseWarning
+        {
+            public string message { get; set; }
+        }
+
+        private class WebApiResponseRecord
+        {
+            public string recordId { get; set; }
+            public Dictionary<string, object> data { get; set; }
+            public List<WebApiResponseError> errors { get; set; }
+            public List<WebApiResponseWarning> warnings { get; set; }
+        }
+
+        private class WebApiEnricherResponse
+        {
+            public List<WebApiResponseRecord> values { get; set; }
+        }
+        #endregion
+
+
+        /// <summary>
+        /// Note that this function can translate up to 1000 characters. If you expect to need to translate more characters, use 
+        /// the paginator skill before calling this custom enricher
+        /// </summary>
+        [FunctionName("Translate")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]HttpRequest req,
+            TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            string recordId = null;
+            string originalText = null;
+            string originalLanguage = null;
+            string translatedText = null;
+
+            string requestBody = new StreamReader(req.Body).ReadToEnd();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+            // Validation
+            if (data?.values == null)
+            {
+                return new BadRequestObjectResult(" Could not find values array");
+            }
+            if (data?.values.HasValues == false || data?.values.First.HasValues == false)
+            {
+                // It could not find a record, then return empty values array.
+                return new BadRequestObjectResult(" Could not find valid records in values array");
+            }
+
+            recordId = data?.values?.First?.recordId?.Value as string;
+            originalText = data?.values?.First?.data?.text?.Value as string;
+            originalLanguage = data?.values?.First?.data?.language?.Value as string;
+
+            if (recordId == null)
+            {
+                return new BadRequestObjectResult("recordId cannot be null");
+            }
+            
+            if( !originalLanguage.Contains("en") )
+            {
+                translatedText = TranslateText(originalText, "en").Result;
+            }
+            else
+            {
+                translatedText = originalText;
+            }
+            
+            // Put together response.
+            WebApiResponseRecord responseRecord = new WebApiResponseRecord();
+            responseRecord.data = new Dictionary<string, object>();
+            responseRecord.recordId = recordId;
+            responseRecord.data.Add("text", translatedText);
+
+            WebApiEnricherResponse response = new WebApiEnricherResponse();
+            response.values = new List<WebApiResponseRecord>();
+            response.values.Add(responseRecord);
+
+            return (ActionResult)new OkObjectResult(response);
+        }
+
+        /// <summary>
+        /// Use Cognitive Service to translate text from one language to antoher.
+        /// </summary>
+        /// <param name="myText">The text to translate</param>
+        /// <param name="destinationLanguage">The language you want to translate to.</param>
+        /// <returns>Asynchronous task that returns the translated text. </returns>
+        async static Task<string> TranslateText(string myText, string destinationLanguage)
+        {
+            string host = "https://api.microsofttranslator.com";
+            string path = "/V2/Http.svc/Translate";
+
+            // NOTE: Replace this example key with a valid subscription key.
+            string key = "[Subscription Key]";
+
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", key);
+
+            List<KeyValuePair<string, string>> list = new List<KeyValuePair<string, string>>() {
+                new KeyValuePair<string, string>(myText, "en-us")
+            };
+
+            StringBuilder totalResult = new StringBuilder();
+
+            foreach (KeyValuePair<string, string> i in list)
+            {
+                string uri = host + path + "?to=" + i.Value + "&text=" + System.Net.WebUtility.UrlEncode(i.Key);
+
+                HttpResponseMessage response = await client.GetAsync(uri);
+
+                string result = await response.Content.ReadAsStringAsync();
+
+                // Parse the response XML
+                System.Xml.XmlDocument xmlResponse = new System.Xml.XmlDocument();
+                xmlResponse.LoadXml(result);
+                totalResult.Append(xmlResponse.InnerText);
+            }
+
+            return totalResult.ToString();
+
+        }
+    }
+}
